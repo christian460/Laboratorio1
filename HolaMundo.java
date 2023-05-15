9dd19274b0b1e43483cca63bd8de809f32739089
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-05-14 20:42:18 -0500

    Captura el nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class HolaMundo{
 	public static void main (String args[]){
+		Scanner sc=new Scanner(System.in);
 		System.out.println("¡Hola Mundo!");
 		System.out.println("Bienvenidos al curso");
+		System.out.println("Ingrese su nombre:");
+		String nom=sc.nextLine();
+		System.out.println("Hola "+nom);
 	}
 
 }
+
